0bcb4b3f610782182b1beae3a7a6e7cee679306a
Oleg <o.petrashev@gmail.com>
2022-04-10 02:38:18 +0600

    ...

--- src/Viewer.js
+++ src/Viewer.js
@@ -1,55 +1,55 @@
-import * as THREE from "three";
+import * as THREE from 'three'
 export default {
   init(data) {
-    this.createResize();
+    this.createResize()
 
-    this.createRenderer(data.renderer);
-    this.createCamera();
-    this.createScene();
-    this.createLight();
+    this.createRenderer(data.renderer)
+    this.createCamera()
+    this.createScene()
+    this.createLight()
 
-    this.update();
+    this.update()
   },
   createRenderer(settings) {
     if (this.renderer) {
-      this.renderer.domElement.parentNode.removeChild(this.renderer.domElement);
+      this.renderer.domElement.parentNode.removeChild(this.renderer.domElement)
 
-      this.renderer.dispose();
+      this.renderer.dispose()
     }
 
-    this.renderer = new THREE.WebGLRenderer(settings);
+    this.renderer = new THREE.WebGLRenderer(settings)
 
-    settings.parent.appendChild(this.renderer.domElement);
+    settings.parent.appendChild(this.renderer.domElement)
 
-    this.renderer.setClearColor(settings.clearColor || "black");
+    this.renderer.setClearColor(settings.clearColor || 'black')
 
-    this.renderer.setPixelRatio(settings.pixelRatio || devicePixelRatio);
+    this.renderer.setPixelRatio(settings.pixelRatio || devicePixelRatio)
 
-    var that = this;
+    var that = this
 
-    this.addResize("resize_render", () => {
+    this.addResize('resize_render', () => {
       that.renderer.setSize(
         that.renderer.domElement.parentNode.offsetWidth,
         that.renderer.domElement.parentNode.offsetHeight
-      );
-    });
-    this.resizePool["resize_render"]();
+      )
+    })
+    this.resizePool['resize_render']()
   },
   createResize() {
-    var that = this;
-    window.addEventListener("resize", () => {
-      that.resize();
-    });
+    var that = this
+    window.addEventListener('resize', () => {
+      that.resize()
+    })
   },
   resizePool: {},
   addResize(name, func) {
-    this.resizePool[name] = func;
+    this.resizePool[name] = func
   },
   removeRize(name) {
-    delete this.resizePool[name];
+    delete this.resizePool[name]
   },
   resize() {
-    for (var key in this.resizePool) this.resizePool[key]();
+    for (var key in this.resizePool) this.resizePool[key]()
   },
   createCamera() {
     this.camera = new THREE.PerspectiveCamera(
@@ -57,43 +57,40 @@ export default {
       this.renderer.domElement.width / this.renderer.domElement.height,
       1,
       100
-    );
+    )
 
-    var that = this;
+    var that = this
 
-    this.addResize("resize_camera", () => {
-      that.camera.aspect =
-        that.renderer.domElement.width / that.renderer.domElement.height;
+    this.addResize('resize_camera', () => {
+      that.camera.aspect = that.renderer.domElement.width / that.renderer.domElement.height
 
-      that.camera.updateProjectionMatrix();
-    });
+      that.camera.updateProjectionMatrix()
+    })
   },
   createScene() {
-    this.scene = new THREE.Scene();
+    this.scene = new THREE.Scene()
   },
   createLight() {
-    this.light1 = new THREE.DirectionalLight(0xffffff, 0.5);
+    this.light1 = new THREE.DirectionalLight(0xffffff, 0.5)
 
-    this.scene.add(this.light1);
+    this.scene.add(this.light1)
 
-    this.light1.position.set(5, 5, 5);
+    this.light1.position.set(5, 5, 5)
   },
   addUpdate(name, func) {
-    this.updatePool[name] = func;
+    this.updatePool[name] = func
   },
   removeUpdate(name) {
-    delete this.updatePool[name];
+    delete this.updatePool[name]
   },
   updatePool: {},
+
   update() {
-    this.renderer.render(this.scene, this.camera);
-
-    var that = this;
-
+    this.renderer.render(this.scene, this.camera)
+    const that = this
     requestAnimationFrame(() => {
-      that.update();
-    });
-
-    for (var key in this.updatePool) this.updatePool[key]();
-  },
-};
+      that.update()
+    })
+    for (var key in this.updatePool) this.updatePool[key]()
+  }
+}
--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
-import "./style.css";
-import * as THREE from "three";
-import Viewer from "./Viewer";
+import './style.css'
+import * as THREE from 'three'
+import Viewer from './Viewer'
 
 export default class {
   constructor() {
@@ -9,23 +9,26 @@ export default class {
         parent: document.body,
         antialias: true,
         alpha: false,
-        clearColor: "gray",
-        pixelRatio: 1,
-      },
-    });
-    this.createObject();
+        clearColor: 'gray',
+        pixelRatio: 1
+      }
+    })
+    this.createObject()
   }
+
   createObject() {
     this.object = new THREE.Mesh(
       new THREE.BoxGeometry(1, 1, 1),
-      new THREE.MeshStandardMaterial({ color: "gray" })
-    );
+      new THREE.MeshStandardMaterial({ color: 'gray' })
+    )
 
-    Viewer.scene.add(this.object);
-    this.object.position.z = -5;
-    var that = this;
-    Viewer.addUpdate("rotate_object", () => {
-      that.object.rotation.y += 0.01;
-    });
+    Viewer.scene.add(this.object)
+    this.object.position.z = -5
+
+    const that = this
+
+    Viewer.addUpdate('rotate_object', () => {
+      that.object.rotation.y += 0.01
+    })
   }
 }
